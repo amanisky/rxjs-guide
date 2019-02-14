fa054d8bd2350f9f3ac5a3af6862c962d9755519
amanisky <amanisky@126.com>
2019-02-14 17:51:38 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
+/* eslint-disable */
+import {Observable} from 'rxjs'
 
-Vue.config.productionTip = false
+/**
+ * 1、创建一个 Observable（可观察对象）
+ * 2、然后让 Observer（观察者）去订阅 Observable
+ * 3、订阅以后就会执行 Observable，它会将数据一个一个的推送给订阅它的 Observer
+ * 4、Observer 定义了一些方法（next、error、complete）决定了怎么样去使用 Observale 推送过来的数据
+ * 
+ * Observable 是数据的生产者
+ * Observer 是数据的消费者
+ */
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//  创建一个可观察对象
+let fruitesObservable = Observable.create(observer => {
+  // next 方法用来将数据一个一个的传递给 Observer
+  observer.next('苹果')
+  observer.next('香蕉')
+  observer.next('橘子')
+  // error 方法会告知 Observer 发生了错误
+  // observer.error()
+
+  // complete 方法会告知 Observer 数据传递完成
+  observer.complete()
+})
+
+// 创建一个观察者
+let fruitesObserver = {
+  next: data => console.log(data),
+  complete: () => console.log('完成!')
+}
+
+// 订阅
+fruitesObservable.subscribe(fruitesObserver)
